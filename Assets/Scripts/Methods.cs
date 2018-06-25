ea300bc6f06832d9e460250bf13d437d70c40b88
Alex Butkov <>
2018-06-25 03:15:51 +0300

    Summaries

--- Assets/Scripts/Methods.cs
+++ Assets/Scripts/Methods.cs
@@ -6,19 +6,31 @@ using UnityEngine.UI;
 
 public class Methods : MonoBehaviour
 {
-    #region Initialization
+    #region ==== Fields ====
+    //----public----
+    /// <summary>(массив стержней)</summary>
     public GameObject[] Spires = new GameObject[spiresQuant];
+    /// <summary>(экземпляр поверхности)</summary>
     [Space(8)]
+    public GameObject Plane;
+    /// <summary>(префаб основания)</summary>
     public GameObject BaseSample;
+    /// <summary>(префаб стержня)</summary>
     public GameObject SpireSample;
+    /// <summary>(префаб диска)</summary>
     public GameObject RingSample;
+    /// <summary>(UI элемент для вывода скорости)</summary>
     [Space(8)]
     public GameObject UiSpeed;
+    /// <summary>(UI элемент для ввода)</summary>
     public InputField UiInput;
+    /// <summary>(UI элемент для вывода)</summary>
     public GameObject UiOutput;
+    /// <summary>(начальный UI элемент)</summary>
     public GameObject UiStart;
+    /// <summary>(конечный UI элемент)</summary>
     public GameObject UiEnd;
-
+    /// <summary>(скорость движения)</summary>
     public float Speed
     {
         get
@@ -31,6 +43,7 @@ public class Methods : MonoBehaviour
             speed = value * RingsQuant;
         }
     }
+    /// <summary>(длина массива дисков)</summary>
     public int RingsQuant
     {
         get
@@ -44,14 +57,13 @@ public class Methods : MonoBehaviour
                 if (value >= 0)
                 {
                     ringsQuant = value;
-                    StartCoroutine(Restart());
+                    Restarter();
                 }
             }
         }
     }
-
-
-    //----
+    //----private----
+    /// <summary>(рамерный множитель)</summary>
     [SerializeField, Space(8)]
     private float scaleMult = 0.5f;
     private const int spiresQuant = 3;
@@ -59,24 +71,36 @@ public class Methods : MonoBehaviour
     private string warning = "Длина этого массива должна быть равна трём!";
     private string message = "преобразование не удалось,\n попробуйте снова...";
     private readonly List<GameObject> Rings = new List<GameObject>();
-    private readonly float waitDivider = 5;
     private Vector3 scaleVector = new Vector3(1,0,1);
     private int ringsQuant = 0 ;
-    private float speed = 20;
+    private float speed;
+    private float startSpeed = 10;
     private Text SpText;
     private Text OutText;
+    private bool isReady;
     #endregion
-    
+    #region ==== Methods ====
+    #region ~~~~ Wrap ~~~~
+    //----public----
+    /// <summary>
+    /// (запускает корутину <see cref="Restart"/>)
+    /// </summary>
     public void Restarter()
     {
         StartCoroutine(Restart());
     }
 
+    /// <summary>
+    /// (запускает корутину <see cref="MovingHandler"/>)
+    /// </summary>
     public void Starter()
     {
         StartCoroutine(MovingHandler());
     }
 
+    /// <summary>
+    /// (проверяет строку введенную пользователем)
+    /// </summary>
     public void CheckString()
     {
         int buffer;
@@ -98,9 +122,10 @@ public class Methods : MonoBehaviour
             OutText.text = message;
         }
     }
-    //---
-    // Use this for initialization
-    void Start()
+    #endregion
+    #region ~~~~ MonoBehaviour ~~~~
+    //----private----
+    private void Start()
     {
         SpText = UiSpeed.GetComponent<Text>();
         OutText = UiOutput.GetComponent<Text>();
@@ -109,8 +134,12 @@ public class Methods : MonoBehaviour
         //---
         for (int i = 0; i < bases.Length; i++)
         {
-            bases[i] = Instantiate(BaseSample,Vector3.down*2,Quaternion.Euler(Vector3.zero));
+            bases[i] = Instantiate(BaseSample);
+            bases[i].transform.position = Vector3.right * Spires[i].transform.position.x ;
+            bases[i].transform.parent = transform;
+            bases[i].name += string.Format(" ({0})", i);
         }
+        Plane.transform.parent = bases[1].transform;
     }
 
     private void OnValidate()
@@ -121,37 +150,46 @@ public class Methods : MonoBehaviour
             Array.Resize(ref Spires, spiresQuant);
         }
     }
-
-    private IEnumerator AddRings()
+    #endregion
+    #region ~~~~ Resize Objects ~~~~
+    /// <summary>
+    /// (создает заданное количество дисков)
+    /// </summary>
+    public IEnumerator AddRings()
     {
         for (int i = RingsQuant - 1 ; i >= 0; i--)
         {
             GameObject ring = Instantiate(RingSample);
             Transform parent = Spires[0].transform;
-            ring.name += string.Format(" ({0})", i);
+            ring.name += string.Format(" ({0})", (RingsQuant - 1 - i));
             ring.transform.parent = parent;
             ring.transform.position = parent.position + Vector3.down * parent.localScale.y + Vector3.up * (RingsQuant - i);
             ring.GetComponentInChildren<MeshRenderer>().material.color = UnityEngine.Random.ColorHSV();
             ring.transform.localScale += scaleVector * i * scaleMult ;
             Rings.Add(ring);
-            yield return new WaitForSeconds(Time.deltaTime / waitDivider);
         }
+        yield break;
     }
 
+    /// <summary>
+    /// (возвращает сцену в начальное состояние)
+    /// </summary>
     private IEnumerator Restart()
     {
+        isReady = false;
         for (int i = Rings.Count - 1 ; i >= 0; i--)
         {
             Destroy(Rings[i]);
-            yield return new WaitForSeconds(Time.deltaTime / waitDivider);
         }
         Rings.Clear();
-        yield return new WaitForSeconds(Time.deltaTime / waitDivider);
-        yield return SetScene();
+        yield return StartCoroutine(SetScene());
         yield return StartCoroutine(AddRings());
-
+        isReady = true;
     }
 
+    /// <summary>
+    /// (модифицирует объекты на сцене)
+    /// </summary>
     private IEnumerator SetScene()
     {
         Vector3 tempVector = Vector3.up * Math.Max(1, RingsQuant);
@@ -163,46 +201,74 @@ public class Methods : MonoBehaviour
             bases[i].transform.localScale = BaseSample.transform.localScale + tempScale / 2;
         }
         Vector3 tempPosition = Spires[1].transform.parent.position + Vector3.up * Spires[1].transform.localScale.y;
-        Vector3 basePosition = Spires[1].transform.parent.position;
         for (int i = 0; i < spiresQuant; i++)
         {
-            if (RingsQuant >= 1)
+            if (RingsQuant >= 0)
             {
                 switch (i)
                 {
                     case 0:
-                        Spires[i].transform.position = tempPosition + Vector3.left * tempMult; 
-                        bases[i].transform.position = basePosition + Vector3.left * tempMult; break;
+                        Spires[i].transform.position = tempPosition + Vector3.left * tempMult; break;
                     case 1:
-                        Spires[i].transform.position = tempPosition; 
-                        bases[i].transform.position = basePosition; break;
+                        Spires[i].transform.position = tempPosition; break;
                     case 2:
-                        Spires[i].transform.position = tempPosition + Vector3.right * tempMult;
-                        bases[i].transform.position = basePosition + Vector3.right * tempMult; break;
+                        Spires[i].transform.position = tempPosition + Vector3.right * tempMult; break; 
                     default:
                         break;
                 }
+                bases[i].transform.position = Vector3.right * Spires[i].transform.position.x;
             }
         }
         yield break;
     }
-
+    #endregion
+    #region ~~~~ Move Objects ~~~~
+    /// <summary>
+    /// (при возможности запускает корутину <see cref="SetMoving"/> и по завершению активирует <see cref="UiEnd"/>)
+    /// </summary>
     private IEnumerator MovingHandler()
     {
-        yield return StartCoroutine(SetMoving(0, Spires[0], Spires[2], Spires[1]));
+        while (!isReady)
+        {
+            yield return null;
+        }
+        speed = RingsQuant * startSpeed;
+        yield return StartCoroutine(SetMoving(Spires[0], Spires[2], Spires[1]));
         UiEnd.SetActive(true);
     }
 
-    private IEnumerator SetMoving(int number, GameObject source, GameObject target, GameObject buffer)
+    /// <summary>
+    /// (определяет порядок перемещений и запускает корутину <see cref="DoMoving"/> необходимое количество раз)
+    /// </summary>
+    /// <param name="source">(начальный стержень)</param>
+    /// <param name="target">(конечный стержень)</param>
+    /// <param name="buffer">(вспомогательный стержень)</param>
+    /// <param name="endIndex">(индекс нижнего диска)</param>
+    private IEnumerator SetMoving(GameObject source, GameObject target, GameObject buffer, uint endIndex = 0)
     {
-        if (number < Rings.Count)
+        if (endIndex < RingsQuant)
         {
-            yield return StartCoroutine(SetMoving(number + 1, source, buffer, target));
-            yield return StartCoroutine(DoMoving(Rings[number].transform, target.transform));
-            yield return StartCoroutine(SetMoving(number + 1, buffer, target, source));
+            for (int i = RingsQuant - 1 ; i >= (int)endIndex; i--)
+            {
+                if (i % 2 == endIndex % 2)
+                {
+                    yield return StartCoroutine(DoMoving(Rings[i].transform, target.transform));
+                    yield return StartCoroutine(SetMoving(buffer, target, source, (uint)i + 1));
+                }
+                else
+                {
+                    yield return StartCoroutine(DoMoving(Rings[i].transform, buffer.transform));
+                    yield return StartCoroutine(SetMoving(target, buffer, source, (uint)i + 1));
+                }
+            }
         }
     }
-    
+
+    /// <summary>
+    /// (реализует перемещение на всех отрезках)
+    /// </summary>
+    /// <param name="ring">(Transform диска)</param>
+    /// <param name="target">(Transform конечного стержня)</param>
     private IEnumerator DoMoving(Transform ring, Transform target)
     {
         yield return StartCoroutine(MovingTowards(ring, ring.parent.position));
@@ -212,14 +278,22 @@ public class Methods : MonoBehaviour
         ring.parent = target;
     }
 
+    /// <summary>
+    /// (реализует перемещение на текущем отрезке)
+    /// </summary>
+    /// <param name="ring">(Transform диска)</param>
+    /// <param name="targetPosition">(Position конечного стержня)</param>
+    /// <returns></returns>
     private IEnumerator MovingTowards(Transform ring, Vector3 targetPosition)
     {
         float step;
-        while (ring.position != targetPosition)
+        while ((targetPosition - ring.position).magnitude > 0.001)
         {
             step = Speed * Time.deltaTime;
             ring.position = Vector3.MoveTowards(ring.position, targetPosition, step);
             yield return null;
         }
     }
+    #endregion
+    #endregion
 }
